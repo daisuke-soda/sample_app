3776cd87b762368ce757aa7defd06c572d9d008a
daisuke-soda <sdobsession23@gmail.com>
2020-08-03 14:58:36 +0900

    [Finish]3-3~3-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8919f0f8723d6e46ed63b09fe710629ab3602ef68798bfac50dd31db3c923febc900d6e6b788ba6d1c0525ea857c7e88236559d6615fb6bb4e662dd13ecbb66f'
